9fc43a2a21162b8b4f5afed7568e2bcde7bb7265
Jose Alonso <j-e-16@hotmail.com>
2026-05-30 16:02:01 +0200

    Primer ejercicio hecho

--- katas-python-resueltas.py
+++ katas-python-resueltas.py
@@ -1,6 +1,8 @@
 #1 Escribe una función que reciba una cadena de texto como parámetro y devuelva un diccionario con las frecuencias de cada letra en la cadena. Los espacios no deben ser considerados.
 
-cadena_texto = input(f'Introduce una oracion o palabra:')
+#Para ejecutar esto con el input, se debe ejecutar el archivo .py desde la terminal. Para usarlo con "Run Code", se puede quitar el input y declarar una cadena de texto predeterminada. ¡Muy importante! GUARDAR LOS CAMBIOS con CTRL+S
+
+cadena_texto = input("Introduce una oracion o palabra")
 
 def frecuencia_letras (cadena_texto):
     frecuencias = {}
