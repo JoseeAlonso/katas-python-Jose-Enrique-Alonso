90ada5609548fe7d27306a735fa56f2461623525
Jose Alonso <j-e-16@hotmail.com>
2026-06-02 22:16:43 +0200

    Ejercicio 3 resuelto

--- katas-python-resueltas.py
+++ katas-python-resueltas.py
@@ -2,7 +2,7 @@
 
 #Para ejecutar esto con el input, se debe ejecutar el archivo .py desde la terminal. Para usarlo con "Run Code", se puede quitar el input y declarar una cadena de texto predeterminada. ¡Muy importante! GUARDAR LOS CAMBIOS con CTRL+S
 
-cadena_texto = input("Introduce una oracion o palabra")
+cadena_texto = input('Introduce una oración o palabra')
 
 def frecuencia_letras (cadena_texto):
     frecuencias = {}
@@ -26,15 +26,41 @@ print(resultado)
 
 #Importante el pasar a lista el resultado ya que lo que arroja el map() es un object.
 
-def funcion (num):
+def lista_doble (num):
     return num*2
 
 numeros = [1,2,3,4,5,6,7,8,9]
 
-resultado_map = map(funcion,numeros)
+resultado_map = map(lista_doble,numeros)
 
 print(f'El resultado de la ejecución de la función map es : {resultado_map}')
 
-lista_resultado = list(map(funcion,numeros))
+lista_resultado = list(map(lista_doble,numeros))
 
-print(f'El resultado despues de convertirlo a lista es: {lista_resultado}')
+print(f'El resultado despues de convertirlo a una lista es: {lista_resultado}')
+
+
+#3 Escribe una función que tome una lista de palabras y una palabra objetivo como parámetros. La función debe devolver una lista con todas las palabras de la lista original que contengan la palabra objetivo.
+
+def lista_objetivo(lista_palabras, palabra_objetivo):
+    
+    resultado = []
+    palabras_encontradas = 0
+    
+    for palabra in lista_palabras:
+        
+        if palabra_objetivo.lower() in palabra.lower():
+            
+            palabras_encontradas += 1
+            
+            resultado.append(palabra)
+            
+    print(f'La palabra {palabra_objetivo} se encuentra {palabras_encontradas} veces y la lista es {resultado}')
+    
+    return resultado
+
+lista_palabras = ['Azul', 'Rojo', 'Verde', 'Amarillo', 'Celeste', 'Morado', 'Verde', 'Verde']
+
+palabra_objetivo = input('Introduce la palabra a buscar: ')
+
+lista_objetivo(lista_palabras, palabra_objetivo)
