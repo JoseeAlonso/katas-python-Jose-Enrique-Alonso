d3e72e4b39459679216051b3222b4417bb0015b9
Jose Alonso <j-e-16@hotmail.com>
2026-06-22 17:53:52 +0200

    Adelanto de ejercicios resueltos

--- Ejercicio-8.py
+++ Ejercicio-8.py
@@ -1,1 +1,13 @@
 #8 Escribe un programa que pida al usuario dos números e intente dividirlos. Si el usuario ingresa un valor no numérico o intenta dividir por cero, maneja esas excepciones de manera adecuada y muestra un mensaje indicando si la división fue exitosa o no.
+
+try:
+    numero_uno = int(input('Introduce un número: '))
+    numero_dos = int(input ('Introduce otro número: '))
+    
+    print(f'División exitosa, resultado: {numero_uno/numero_dos}')
+    
+except ZeroDivisionError:
+    print(f'No es posible dividir entre {numero_dos}')
+    
+except ValueError:
+    print('Error. Debes introducir un número')
