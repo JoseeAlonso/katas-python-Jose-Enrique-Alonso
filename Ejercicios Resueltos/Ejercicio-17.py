d1085929adf8e9af2337b6d0753e3744f117a0f0
Jose Alonso <j-e-16@hotmail.com>
2026-06-23 21:20:27 +0200

    Adelanto de ejercicios resueltos

--- Ejercicios Resueltos/Ejercicio-17.py
+++ Ejercicios Resueltos/Ejercicio-17.py
@@ -1,1 +1,12 @@
+from functools import reduce
+
 #17 Crea una función que tome una lista de dígitos y devuelva el número correspondiente. Por ejemplo, [5,7,2] corresponde al número 572. Usa la función reduce().
+
+lista_digitos = [5,7,2,6,8] 
+
+def funcion_digitos (lista):
+    
+    return reduce(lambda acumulador, digito: acumulador * 10 + digito, lista)
+
+resultado = funcion_digitos(lista_digitos)
+print(resultado)
---
+++ Ejercicios Resueltos/Ejercicio-18.py
@@ -0,0 +1,2 @@
+#18 Escribe un programa en Python que cree una lista de diccionarios con información de estudiantes (nombre, edad, calificación) y use filter para extraer a los estudiantes con una calificación mayor o igual a 90.
+
