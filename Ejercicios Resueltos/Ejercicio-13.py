06b9836d4cc3e9471077a07ed0dd01119bcd6ec7
Jose Alonso <j-e-16@hotmail.com>
2026-06-23 17:50:21 +0200

    Adelanto de ejercicios resueltos

---
+++ Ejercicios Resueltos/Ejercicio-13.py
@@ -0,0 +1,2 @@
+#13 Genera una función que, para un conjunto de caracteres, devuelva una lista de tuplas con cada letra en mayúsculas y minúsculas. Las letras no pueden estar repetidas. Usa la función map().
+
